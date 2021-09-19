75fcac86355fbd78ad056ed0a5a974572ec81485
Malte <malte.hoch@cgm.com>
2021-09-19 09:32:12 +0200

    Added Shadow DOM

--- components.js
+++ components.js
@@ -2,27 +2,40 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         
-        this._tooltipContainer; // Globale Variable
+        /* Global Variablen werden hier bloß gesetzt oder defaulted. Der constructor ist unabhängig des DOMs,
+        daher der connectedCallback() */
+        
+        this._tooltipContainer;
+        this._tooltipText = 'The default text';
+
+        /* Shadow DOM */
+
+        this.attachShadow({ mode: 'open' });
 
         console.log('<udemy-tooltip> created.');
     }
 
     connectedCallback() {
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = '(?) ';
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this)); // Wird nur an die Klasse gebunden
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this)); // Wird nur an die Klasse gebunden
-        this.appendChild(tooltipIcon);
+         
+        this.shadowRoot.appendChild(tooltipIcon);
     }
 
     _showTooltip() { // _ indiziert für JS eine private Methode
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is the tooltip text!';
-        this.appendChild(this._tooltipContainer);
+        this._tooltipContainer.textContent = this._tooltipText;
+
+        this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
     _hideTooltip() {
-        this.removeChild(this._tooltipContainer);
+        this.shadowRoot.removeChild(this._tooltipContainer);
     }
 }
 
