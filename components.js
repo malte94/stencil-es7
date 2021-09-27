e43de3bd13abeb28e8ec7fff28ffcb581de74a06
Malte Hoch <mhoch94@gmail.com>
2021-09-27 17:28:55 +0200

    disconnectedCallback, attributeChangedCallback & Refactoring

--- components.js
+++ components.js
@@ -2,11 +2,11 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         
-        /* Global Variablen werden hier bloß gesetzt oder defaulted. Der constructor ist unabhängig des DOMs,
+        /* Private Klassenvariablen werden hier bloß gesetzt oder defaulted. Der constructor ist unabhängig des DOMs,
         daher der connectedCallback() */
         
-        this._tooltipContainer;
         this._tooltipText = 'The default text';
+        this._tooltipIcon;
 
         /* Shadow DOM & Template */
 
@@ -16,31 +16,46 @@ class Tooltip extends HTMLElement {
 
         <style>
             div {
-                background-color: black;
-                color: white;
-                position: absolute;
-                z-index: 10;
+              font-weight: normal;
+              background-color: black;
+              color: white;
+              position: absolute;
+              top: 1.5rem;
+              left: 0.75rem;
+              z-index: 10;
+              padding: 0.15rem;
+              border-radius: 3px;
+              box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
             }
 
-        :host(.important) {
-            background: #ccc;
-        }
+            :host(.important) {
+              background: var(--color-primary, #ccc);
+              padding: 0.15rem;
+            }
 
-        :host-context(p) {
-        font-weight: bold;
-        }
+            :host-context(p) {
+              font-weight: bold;
+            }
 
-        .highlight {
-        background-color: red;
-        }
+            .highlight {
+              background-color: red;
+            }
 
-        ::slotted(.highlight) {
-        border-bottom: 1px dotted red;
-        }
+            ::slotted(.highlight) {
+              border-bottom: 1px dotted red;
+            }
 
+            .icon {
+              background: black;
+              color: white;
+              padding: 0.15rem 0.5rem;
+              text-align: center;
+              border-radius: 50%;
+            }
         </style>
+
         <slot>Some default</slot>
-        <span> (?)</span>
+        <span class="icon">?</span>
     `;
 
         console.log('<udemy-tooltip> created.');
@@ -48,23 +63,61 @@ class Tooltip extends HTMLElement {
 
     connectedCallback() {
         if (this.hasAttribute('text')) {
-            this._tooltipText = this.getAttribute('text');
-          }
-          const tooltipIcon = this.shadowRoot.querySelector('span');
-          tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this)); // Wird nur an die Klasse gebunden
-          tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this)); // Wird nur an die Klasse gebunden
-          this.shadowRoot.appendChild(tooltipIcon);
+          this._tooltipText = this.getAttribute('text');
+        }
+        this._tooltipIcon = this.shadowRoot.querySelector('span');
+        this._tooltipIcon.addEventListener(
+          'mouseenter',
+          this._showTooltip.bind(this)
+        );
+        this._tooltipIcon.addEventListener(
+          'mouseleave',
+          this._hideTooltip.bind(this)
+        );
+        this.style.position = 'relative';
+        this._render();
+      }
 
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) {
+            return;
+        }
+        if (name === 'text') {
+            this._tooltipText = newValue;
+        }
     }
 
-    _showTooltip() { // _ indiziert für JS eine private Methode
-        this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = this._tooltipText;
-        this.shadowRoot.appendChild(this._tooltipContainer);
+    static get observedAttributes() {
+        return ['text'];
     }
 
+    disconnectedCallback() { // Cleanup, wenn Klasse entfernt wird
+        console.log('Disconnceted');
+        this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
+        this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip);
+    }
+
+    _render() {
+        let tooltipContainer = this.shadowRoot.querySelector('div');
+        if (this._tooltipVisible) {
+            tooltipContainer = document.createElement('div');
+            tooltipContainer.textContent = this._tooltipText;
+            this.shadowRoot.appendChild(tooltipContainer);
+        } else {
+            if (tooltipContainer) {
+            this.shadowRoot.removeChild(tooltipContainer);
+            }
+        }
+    }
+
+    _showTooltip() {
+        this._tooltipVisible = true;
+        this._render();
+      }
+    
     _hideTooltip() {
-        this.shadowRoot.removeChild(this._tooltipContainer);
+    this._tooltipVisible = false;
+    this._render();
     }
 }
 
